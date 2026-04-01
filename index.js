4b04e5d627aec1d238048908ef74a2ac07365db3
pratyush180403 <dubeypratyush9999@gmail.com>
2026-04-02 04:20:34 +0530

    js

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log("hello")
+// const newPen = {
+// fullName : "Parker",
+// penColor : "black",
+// price : 270 , 
+// ratings : 4.9,
+// isDeal : true,
+// discount : true,
+// };
+
+// console.log(newPen)
+// alert("hellooo!!!!!!!!");
+// let nums = prompt("enter your number");
+// if(nums%5===0){
+//     console.log(nums,"is a multiple of 5")
+// }else{
+//     console.log(nums, "is not a multiple of 5")
+// }
+// let marks = prompt("enter your marks");
+// if(100>=marks && marks>=80){
+//     console.log("your grade is A")
+// }else if(79>=marks && marks>=70){
+//     console.log("your grade is B")
+// }else if(69>=marks && marks>=60){
+//     console.log("your grade is C")
+// }else if(59>=marks && marks>=50){
+//     console.log("your grade is D")
+// }else {
+//     console.log("you have failed the exam")
+// }
+// let string = "apna lund";
+// for(let i of string){
+    
+//     console.log("i=" , i)
+// }
+let student = {
+    name: "rahul",
+    age : 20,
+    cgpa : 9.9,
+    isPass : true,
+
+};
+for (let key in student){
+    console.log("key = ", key , ", value = ", student[key]);
+}
---
+++ operators.js
@@ -0,0 +1,53 @@
+// let a = 10;
+// let b = 5;
+// console.log("a= ", a , "b= ",b);
+// console.log("a+b= " , a+b);
+// console.log("a-b= " , a-b);
+// console.log("a/b= " , a/b);
+// console.log("a*b= " , a*b);
+// console.log("a%b= " , a%b);
+// console.log("a**b= " , a**b);
+// a++;
+// b--;
+// console.log("a++= " , ++a);
+// console.log("b--= " , --b);
+// let a = 5;
+// let b = 6;
+// console.log((a>b)&&(a<b))
+// let age = 25;
+// if(age>18){
+//     console.log("can vote");
+// }
+// let mode = "dark";
+// let color;
+// if(mode==="dark"){
+//     color = "black";
+// }
+
+// if(mode==="light"){
+//     color = "white";
+// }
+// console.log(color)
+// let a = "9" ;
+// if(a%2===0){
+//     console.log("a is even");
+// } 
+// if(a%2!==0){
+//     console.log("a is odd");
+// }
+// age = 2;
+// let result = age>18 ? "adult" : "minor";
+// console.log(result)
+// function getFee(isMember) {
+//   return isMember ? "$2.00" : "$10.00";
+// }
+
+// console.log(getFee(true));
+// // Expected output: "$2.00"
+
+// console.log(getFee(false));
+// // Expected output: "$10.00"
+
+// console.log(getFee(null));
+// // Expected output: "$10.00"
+alert("helloooo!!");
