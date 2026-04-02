33c9ac2ea491f3cb66cb852a7d7bca3359f43322
pratyush180403 <dubeypratyush9999@gmail.com>
2026-04-03 03:56:59 +0530

    commits

--- index.js
+++ index.js
@@ -32,13 +32,83 @@
     
 //     console.log("i=" , i)
 // }
-let student = {
-    name: "rahul",
-    age : 20,
-    cgpa : 9.9,
-    isPass : true,
+// let student = {
+//     name: "rahul",
+//     age : 20,
+//     cgpa : 9.9,
+//     isPass : true,
 
-};
-for (let key in student){
-    console.log("key = ", key , ", value = ", student[key]);
-}
+// };
+// for (let key in student){
+//     console.log("key = ", key , ", value = ", student[key]);
+// }
+
+//print all even no from 0 to 100//
+// let n = 1;
+// for(let i = 0 ; i<= 50 ; i++){
+//     n = i*2;
+//     console.log(n);
+// }
+
+
+// practice question no 2//
+
+// let gameNum = "100";
+// //mistake yaad rakhna ki gameNum 1 string return karta hai (prompt), so use "100"
+// let userNum = prompt("guess the right number");
+
+// while(userNum != gameNum){
+    
+//     userNum=prompt("you entered the incorrect number , try again");
+// }
+// console.log("yayyyyyy! , you entered the correct number")
+// let obj = {
+//     item: "pen",
+//     price: 10,
+//     discount : 4,
+// }
+// console.log(`the price of the ${obj.item} is ${obj.price} rupees`)
+// let str = "0123456712389";
+
+// console.log(str.replaceAll("123" ,"111" ))
+// let name = prompt("enter your name");
+// str1 = "@";
+// str3 = name.length;
+// strMain = str1+name.concat(str3);
+// console.log(strMain)
+
+
+// let arr = [ "shyam" , "ram", "modiji" , "putin" , "kimjom"];
+// arr[3] = "rasputin";
+// for(let i = 0 ; i < arr.length ; i++){
+//     console.log("element" , i+1, "is" ,arr[i])
+// }
+// for(let el of arr){
+//     console.log(el.toUpperCase());
+// }
+
+//////ques//
+
+// let arr = [85, 97, 44 , 37, 76 , 60];
+//  arrSum = 0;
+// for(let i = 0 ; i < arr.length ; i++){
+//     arrSum = arrSum + arr[i];
+// }
+// console.log(arrSum/arr.length)
+
+// let arr = [250 , 645, 300, 900, 50];
+// for(let i = 0 ; i < arr.length ; i++){
+//     arr[i] = arr[i]*0.9 ;
+// }
+// console.log(arr.pop())
+
+// let comp= ["bloomberg", "microsoft" , "uber" , "google" , "ibm" , "netflix"];
+// // comp.splice(0 , 1);
+// // console.log(comp)
+
+// comp.splice(2 , 1 , "ola");
+// console.log(comp)
+
+// comp.push("amazon");
+// console.log(comp)
+
